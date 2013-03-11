40f8922b48528ea38c7dd4b785eb471ffbdffbfd
Christopher Brown <io@henrian.com>
2013-03-11 15:29:16 -0500

    Cleanup with commas, and update forex lib

--- onefe.js
+++ onefe.js
@@ -1,41 +1,38 @@
 #!/usr/bin/env node
-var __ = require('underscore')._,
-    fs = require('fs'),
-    path = require('path'),
-    http = require('http'),
-    amulet = require('amulet'),
-    wrappers = require('wrappers'),
-    argv = require('optimist').argv,
-    redis = require('redis').createClient(),
-    sub_redis = require('redis').createClient(),
-    currencies = require('./currencies');
-    
-var host = argv.host || '127.0.0.1',
-    port = argv.port || 3261;
+var fs = require('fs');
+var path = require('path');
+var http = require('http');
+var amulet = require('amulet');
+var redis = require('redis');
+var currencies = require('./currencies');
+var argv = require('optimist').default({port: 3261, hostname: '127.0.0.1'}).argv;
+
+var main_redis = redis.createClient();
+var pubsub_redis = redis.createClient();
 
 amulet.set({minify: true, root: path.join(__dirname, 'templates')});
 
 var all_rates = {}, most_recent;
-redis.keys('forex:*', function(err, rate_keys) {
-  redis.mget(rate_keys, function(err, cached_rates) {
+main_redis.keys('forex:*', function(err, rate_keys) {
+  main_redis.mget(rate_keys, function(err, cached_rates) {
     for (var i = 0, l = rate_keys.length; i < l; i++) {
       all_rates[rate_keys[i]] = JSON.parse(cached_rates[i]);
     }
   });
 });
 
-sub_redis.on('message', function (channel, message) {
+pubsub_redis.on('message', function (channel, message) {
   var payload = JSON.parse(message);  // message = {date: date_iso8601, rates: payload.rates}
-  console.log("Got sub_redis message from worker: " + payload.date.toString());
+  console.log('Got pubsub_redis message from worker: ' + payload.date.toString());
   all_rates[payload.date] = payload.rates;
   most_recent = payload.rates;
 });
-sub_redis.subscribe('forex');
+pubsub_redis.subscribe('forex');
 
 function refreshMostRecent() {
   console.log('refreshMostRecent');
-  var keys = Object.keys(all_rates).sort(),
-    most_recent_key = keys[keys.length - 1];
+  var keys = Object.keys(all_rates).sort();
+  var most_recent_key = keys[keys.length - 1];
   most_recent = all_rates[most_recent_key];
 }
 
@@ -50,18 +47,18 @@ http.createServer(function(req, res) {
           rates: all_rates[key]
         };
       });
-    res.writeHead(200, {"Content-Type": "application/json"});
+    res.writeHead(200, {'Content-Type': 'application/json'});
     res.end(JSON.stringify(historical_rates));
     console.timeEnd(req.url);
   }
   else {
     if (!most_recent) refreshMostRecent();
     var context = {rates: most_recent, currencies: currencies};
-    res.writeHead(200, {"Content-Type": "text/html"});
+    res.writeHead(200, {'Content-Type': 'text/html'});
     amulet.render(res, ['layout.mu', 'index.mu'], context, function() {
       console.timeEnd(req.url);
     });
   }
-}).listen(port, host, function() {
-  console.log(__filename + ' server running on ' + host + ':' + port);
+}).listen(argv.port, argv.host, function() {
+  console.log(__filename + ' server running on ' + argv.host + ':' + argv.port);
 });
--- rates_worker.js
+++ rates_worker.js
@@ -1,27 +1,30 @@
-var exchange = require('exchange-rates'),
-  redis = require('redis').createClient(),
-  pub_redis = require('redis').createClient(),
-  seconds_per_month = 60*60*24*31,
-  queries_per_month = 1000,
-  seconds_per_query = seconds_per_month / queries_per_month;
+var exchange = require('open-exchange-rates');
+var redis = require('redis');
+var seconds_per_month = 60*60*24*31;
+var queries_per_month = 1000;
+var seconds_per_query = seconds_per_month / queries_per_month;
 
-// seconds_per_query = 3;
-// Everytime we fetch rates from an API, we'll store them as a redis key: "forex:YYYY-MM-DDTHH:MM:SS"
+exchange.set({app_id: '28967c4d6b86479bb36a407440bc6f0c'});
+
+var pub_redis = redis.createClient();
+// Everytime we fetch rates from an API, we'll store them as a redis key: 'forex:YYYY-MM-DDTHH:MM:SS'
 //   that is, with the date in UTC ISO-8601, at the second.
 // every 44.64 minutes, except that setInterval takes its timeout in milliseconds
-setInterval(function() {
-  // console.log('Fetching...');
-  exchange.load(function(err, rates_json) {
-    if (err) console.log(err);
-    var payload = JSON.parse(rates_json),
-      date = new Date(payload.timestamp*1000),
-      date_iso8601 = date.toISOString().split('.')[0],
-      redis_key = 'forex:' + date_iso8601;
-    redis.set(redis_key, JSON.stringify(payload.rates), function(err) {
-      if (err) console.log(err);
+function fetch() {
+  exchange.load(function(err) {
+    if (err) console.error(err);
+    var date = new Date(exchange.timestamp);
+    var date_iso8601 = date.toISOString().split('.')[0];
+    var redis_key = 'forex:' + date_iso8601;
+
+    redis.set(redis_key, JSON.stringify(exchange.rates), function(err) {
+      if (err) console.error(err);
       console.log('Fetched rates into key: ' + redis_key);
-      pub_redis.publish("forex", JSON.stringify({date: date_iso8601, rates: payload.rates}));
+      pub_redis.publish('forex', JSON.stringify({date: date_iso8601, rates: exchange.rates}));
     });
   });
-}, seconds_per_query*1000);
-console.log('Fetching every ' + seconds_per_query + ' seconds.');
+}
+
+setInterval(fetch, seconds_per_query*1000);
+console.log('Fetching now and every ' + seconds_per_query + ' seconds.');
+fetch();
